29174587cf2ca2c30b4900c9dd90dce79e34fda1
TonGonzalez <ayrtongonzalez1@gmail.com>
2025-07-20 20:42:12 -0300

    subindoOdesafio

---
+++ Ninja.java
@@ -0,0 +1,17 @@
+package Desafios;
+
+public class Ninja {
+
+    String nome;
+    int idade;
+    String missao;
+    String statusMissao;
+
+    public String toString() {
+        return
+                "Nome:"+ nome +
+                        "\nIdade: " + idade +
+                        "\nMissão: " + missao +
+                        "\nStatus da Missão: " + statusMissao;
+    }
+}
